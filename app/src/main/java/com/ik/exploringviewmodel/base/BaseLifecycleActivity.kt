34db6dcf740f6638d671c3ffe5b00c65d12555b8
Minsuk Eom <boxresin@gmail.com>
2017-07-07 12:26:44 +0900

    Code enhancement

--- app/src/main/java/com/ik/exploringviewmodel/base/BaseLifecycleActivity.kt
+++ app/src/main/java/com/ik/exploringviewmodel/base/BaseLifecycleActivity.kt
@@ -4,7 +4,6 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LifecycleRegistry
 import android.arch.lifecycle.LifecycleRegistryOwner
 import android.arch.lifecycle.ViewModelProviders
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
@@ -15,14 +14,9 @@ abstract class BaseLifecycleActivity<T : AndroidViewModel> : AppCompatActivity()
 
     abstract val viewModelClass: Class<T>
 
-    protected lateinit var viewModel: T
+    protected val viewModel by lazy { ViewModelProviders.of(this).get(viewModelClass)!! }
 
     private val registry = LifecycleRegistry(this)
 
     override fun getLifecycle(): LifecycleRegistry = registry
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(viewModelClass)
-    }
 }
